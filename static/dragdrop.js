23e93e6949310d5cc3b9d3fe2d33fc6e5aa46c31
nakevin004@naver.com <nakevin004@naver.com>
2023-08-11 01:40:20 +0900

    still dealing with drag and drop

--- static/dragdrop.js
+++ static/dragdrop.js
@@ -1,39 +1,28 @@
 fetchImage()
 var item = document.querySelector('.item');
-item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragstart', () => {
+    setTimeout(() => item.classList.add("dragging"), 0);
+});
+item.addEventListener('dragend', () => item.classList.remove("dragging"));
 
-
-function allowDrop(ev) {
-  ev.preventDefault();
-  }
-
-function dragstart(ev) {
-  ev.dataTransfer.setData("text/plain", ev.target.id);
+const sortableList = document.querySelector(".bottom");
+console.log(sortableList)
+const items = sortableList.querySelectorAll(".item");
+const initSortableList = (e) => {
+    e.preventDefault();
+    const draggingItem = document.querySelector(".dragging");
+    // Getting all items except currently dragging and making array of them
+    let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+    // Finding the sibling after which the dragging item should be placed
+    let nextSibling = siblings.find(sibling => {
+      return e.clientX <= sibling.offsetLeft + sibling.offsetWidth / 2;
+    });
+    // Inserting the dragging item before the found sibling
+    sortableList.insertBefore(draggingItem, nextSibling);
 }
+sortableList.addEventListener("dragover", initSortableList);
 
-function drop(ev) {
-  if (ev.target.classList.contains('bottom')) {
-    var data = ev.dataTransfer.getData("text/plain");
-    var node = document.getElementById(data);
-    ev.target.appendChild(node);
-    node.draggable = false;
-    ev.preventDefault();
-    // Create a new item div in the top div
-    var topDiv = document.querySelector('.top');
-    var newItem = document.createElement('div');
-    newItem.className = 'item';
-    newItem.id = 'newItem';
-    newItem.draggable = true;
-    newItem.innerHTML = '<img class="item_img" alt="Random Image"><div class="description"></div>';
-    topDiv.appendChild(newItem);
-
-    // Add event listener for dragstart
-    newItem.addEventListener('dragstart', dragstart);
-
-    // Fetch new image and metadata
-    fetchImage();
-  }
-}
+sortableList.addEventListener("drop", (e) => {e.preventDefault(); fetchImage();});
 
 function fetchImage() {
   fetch('/img')
