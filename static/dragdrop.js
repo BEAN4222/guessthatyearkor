3f1397020922e774f3f15eed808ea69f07e2e172
nakevin004@naver.com <nakevin004@naver.com>
2023-08-28 22:57:56 +0900

    showing the life value

--- static/dragdrop.js
+++ static/dragdrop.js
@@ -16,11 +16,10 @@ const initSortableList = (e) => {
     let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
     // Finding the sibling after which the dragging item should be placed
     let nextSibling = siblings.find(sibling => {
-      return e.clientX <= sibling.offsetLeft + sibling.offsetWidth / 2;
+      return e.clientX <= sibling.offsetLeft + sibling.offsetWidth;
     });
     // Inserting the dragging item before the found sibling
     sortableList.insertBefore(draggingItem, nextSibling);
-    console.log('tlqkf anjdi')
 }
 
 const handleDragLeave = (e) => {
@@ -28,7 +27,6 @@ const handleDragLeave = (e) => {
   if (e.currentTarget.contains(e.relatedTarget)) return;
   const top = document.querySelector(".top");
   top.insertBefore(draggingItem, top.firstChild);
-  console.log('leave')
 }
 
 sortableList.addEventListener("dragover", initSortableList);
@@ -43,6 +41,7 @@ try {
     if (droppedElement && droppedElement.classList.contains("item")) {
       console.log("Item dropped");
       let currentYear = parseInt(droppedElement.querySelector('.description p').textContent);
+      let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
       droppedElement.classList.remove("dragging");
       droppedElement.setAttribute('draggable','false');
       const top = document.querySelector(".top");
@@ -55,30 +54,30 @@ try {
     `;
     // scoring part
     let scoreflag = true
-    let siblings = [...sortableList.querySelectorAll(".item")];
+    
+    console.log(siblings)
     let nextSibling = siblings.find(sibling => {
-      return e.clientX <= sibling.offsetLeft + sibling.offsetWidth / 2;
+      return e.clientX <= sibling.offsetLeft + sibling.offsetWidth;
     });
     let prevSibling = siblings.find(sibling => {
-      return e.clientX >= sibling.offsetLeft + sibling.offsetWidth / 2;
+      return e.clientX >= sibling.offsetLeft + sibling.offsetWidth;
     });
     if (nextSibling) {
       const nextSiblingYear = parseInt(nextSibling.querySelector('.description p').textContent);
+      console.log(1,nextSiblingYear)
       if (nextSiblingYear<currentYear){
         scoreflag = false
       }
     }
     if (prevSibling) {
       const prevSiblingYear = parseInt(prevSibling.querySelector('.description p').textContent);
-      console.log(prevSiblingYear)
+      console.log(2,prevSiblingYear)
       if (prevSiblingYear>currentYear){
         scoreflag = false
       }
     }
-    console.log(scoreflag)
-    if (scoreflag!=true){
+      if (scoreflag!=true){
       life -= 1
-      console.log(life)
       if (life==0){
         alert("You died! Click OK to replay.");
         location.reload();
@@ -92,12 +91,12 @@ try {
       setTimeout(() => item.classList.add("dragging"), 0);
 });
       fetchImage();
+      document.querySelector(".life").textContent = `Life: ${life}`;
     }
   });
   } catch (error) {
     console.log(error);
   }
-
   function fetchImage() {
     let availableElement;
     fetch('/img')
